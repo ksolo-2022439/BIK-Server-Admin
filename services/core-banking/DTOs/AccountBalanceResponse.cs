0788a731fd6c73dd106b1f56e10ba1a533d4b3d1
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2026-03-03 09:14:36 -0600

    refactor: Limpiar Carpetas

--- services/core-banking/DTOs/AccountBalanceResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace BIK.CoreBanking.DTOs {
-    public class AccountBalanceResponse {
-        public int AccountId { get; set; }
-        public decimal Balance { get; set; } 
-    }
-}
--- services/server-admin/configs/cors-configuration.js
+++
@@ -1,8 +0,0 @@
-const corsOptions = {
-    origin: true,
-    credenciales: true,
-    mehtods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-}
- 
-export { corsOptions }
